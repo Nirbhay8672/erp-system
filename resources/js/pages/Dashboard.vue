bc1a6d1aa23e6c634836a515f4ff83d3bc471d19
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2024-08-26 18:22:24 +0530

    Test Revert

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -40,24 +40,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 mt-sm-0 mt-4">
-                        <div class="card  mb-2">
-                            <div class="card-header p-3 pt-2">
-                                <div
-                                    class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
-                                    <i class="material-icons fa fa-user-secret opacity-10"></i>
-                                </div>
-                                <div class="text-end pt-1">
-                                    <p class="text-sm mb-0 text-capitalize">Designations</p>
-                                    <h4 class="mb-0">{{ $page.props.total_designations }}</h4>
-                                </div>
-                            </div>
-                            <hr class="dark horizontal my-0">
-                            <div class="card-footer p-3 text-center">
-                                <a :href="`${$page.props.url}/designations/index`" class="btn btn-outline-primary btn-sm mb-0">View</a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
